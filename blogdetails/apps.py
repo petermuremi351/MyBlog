4fa20fce12898f15e111e480306979fe6d230fb3
Muremi Peter <petermuremi351@gmail.com>
2026-03-27 12:09:34 +0300

    crude operations user authentication....

---
+++ blogdetails/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogdetailsConfig(AppConfig):
+    name = 'blogdetails'
--- myblog/urls.py
+++ myblog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('blogdetails/', include('django.contrib.auth.urls')),
+    path('blogdetails/', include('blogdetails.urls')),
 ]
