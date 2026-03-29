d72ddad1a6c808d8d8867596a15476144b1fb12b
Muremi Peter <petermuremi351@gmail.com>
2026-03-29 10:24:11 +0300

    categories

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -13,16 +13,27 @@ class HomeView(ListView):
     # ordering = ['-id']
     ordering = ['-post_date']
 
+def CategoryView(request, categoryblog):
+    category_posts = Post.objects.filter(category=categoryblog.replace('-', ' '))
+    return render(request, 'categories.html', {'categoryblog':categoryblog.title().replace('-', ' '), 'category_posts':category_posts})  
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
 
 class AddPostView(CreateView):
     model = Post
-    # form_class = PostForm
+    form_class = PostForm
     template_name = 'add_post.html'   
-    fields = '__all__' 
+    # fields = '__all__' 
     # fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category
+    # form_class = PostForm
+    template_name = 'add_category.html'   
+    fields = '__all__' 
+    # fields = ('title', 'body')    
 
 class UpdatePostView(UpdateView):
     model = Post
